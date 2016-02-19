8d981a7d2d7bc7b6688185130432da8134126b17
Adam Buczynski <me@adambuczynski.com>
2016-02-19 13:02:12 +1300

    HTML clean templates

--- build/utils/templates-module-stream.js
+++ build/utils/templates-module-stream.js
@@ -5,6 +5,7 @@
  */
 let gulp = require('gulp');
 let angularTemplateCache = require('gulp-angular-templatecache');
+let htmlclean = require('gulp-htmlclean');
 let angularModuleName = require('./angular-module-name');
 let config = require('../config');
 
@@ -18,6 +19,7 @@ const APP_HTML_SRC = config.APP_HTML_SRC;
  */
 module.exports = function templatesModuleStream() {
   return gulp.src(APP_HTML_SRC)
+    .pipe(htmlclean())
     .pipe(angularTemplateCache({
       module: angularModuleName('Templates'),
       standalone: true
